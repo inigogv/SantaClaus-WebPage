9f7a2e24b1b2e87a1a3a9b98fde83b3511a647e9
inigo <100461095@alumnos.uc3m.es>
2024-10-26 00:03:58 +0200

    registro

--- ej/script/registro_popup.js
+++ ej/script/registro_popup.js
@@ -1,3 +1,15 @@
 function registro() {
     document.getElementById("registro_popup").style.display = "flex";
 }
+
+function cancelar_registro() {
+    if (confirm('¿Estás seguro de que deseas cancelar el registro?')) {
+        document.getElementById('registro_popup').style.display = 'none';
+    }
+}
+
+function limpiar_registro() {
+    if (confirm('¿Estás seguro de que deseas limpiar todos los campos?')) {
+        document.getElementById('formulario_popup').reset();
+    }
+}
