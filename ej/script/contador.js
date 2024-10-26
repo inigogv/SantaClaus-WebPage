f49c4d64eed6e96391c793188652e2aaea37a8a3
oliviagrima <100474858@alumnos.uc3m.es>
2024-10-26 14:36:34 +0200

    mensaje error

--- ej/script/contador.js
+++ ej/script/contador.js
@@ -4,7 +4,7 @@ let actualizar_tiempo_restante = null;
 function contador(){
     navidad = new Date("December 24, 2024 23:59:00").getTime();
     actualizar_tiempo_restante = setInterval(actualizar_tiempo,1000);
-};
+}
 
 function actualizar_tiempo(){
     const tiempo_restante = navidad - Date.now();
@@ -22,6 +22,6 @@ function actualizar_tiempo(){
 
     document.getElementById("contador").innerHTML = `${dias} días ${horas}h ${minutos}min ${segundos}s`;
 
-};
+}
 
 window.onload = contador;
--- ej/script/registro_popup.js
+++ ej/script/registro_popup.js
@@ -3,13 +3,34 @@ function registro() {
 }
 
 function cancelar_registro() {
-    if (confirm('¿Estás seguro de que deseas cancelar el registro?')) {
-        document.getElementById('registro_popup').style.display = 'none';
+    if (confirm("¿Estás seguro de que deseas cancelar el registro?")) {
+        document.getElementById("registro_popup").style.display = "none";
     }
 }
 
 function limpiar_registro() {
-    if (confirm('¿Estás seguro de que deseas limpiar todos los campos?')) {
-        document.getElementById('formulario_popup').reset();
+    if (confirm("¿Estás seguro de que deseas limpiar todos los campos?")) {
+        document.getElementById("formulario_popup").reset();
     }
 }
+
+const restricciones_contraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2,})(?=.*[!@#$%^&*()_\-+=]).{12,}$/;
+
+function validar_contraseña() {
+    const contraseña = document.getElementById("contraseña_usuario");
+    const mnsj_error = document.getElementById("mensaje_de_error");
+
+    contraseña.addEventListener("input", function() {
+        validar_contenido(contraseña, mnsj_error);
+    });
+}
+
+function validar_contenido(contraseña, mnsj_error) {
+    if (!restricciones_contraseña.test(contraseña.value)) {
+        mnsj_error.style.display = "block";
+    } else {
+        mnsj_error.style.display = "none";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", validar_contraseña);
