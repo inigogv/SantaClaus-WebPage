10d35a1e0bc12704e81af044be5b676c4f962766
inigo <100461095@alumnos.uc3m.es>
2024-10-26 16:03:36 +0200

    formulario hijos

--- ej/script/registro_popup.js
+++ ej/script/registro_popup.js
@@ -14,6 +14,35 @@ function limpiar_registro() {
     }
 }
 
+function formulario_hijos() {
+    const num_hijos = document.getElementById('num_hijos').value;
+    const  datos_hijos= document.getElementById('formulario_hijos');
+    datos_hijos.innerHTML = '';
+
+    for (let i = 1; i <= num_hijos; i++) {
+        const campo = `
+            <div class="hijo">
+                <p class="titulo_hijo">Hijo/hija ${i}</p>
+                <div class="nom_edad_hijo">
+                    <div class="caja_label_popup">
+                        <label class= "etiqueta_cajas_popup" for="nombre_hijo_${i}">Nombre</label>
+                        <input class = "caja_escribir" type="text" id="nombre_hijo_${i}" minlength="3" required placeholder="Nombre">
+                    </div>
+                    <div class="caja_label_popup">
+                        <label class= "etiqueta_cajas_popup" for="edad_hijo_${i}">Edad</label>
+                        <input class = "caja_escribir" type="number" id="edad_hijo_${i}" min="0" required placeholder="Edad">
+                    </div>
+                </div>
+                <div class="caja_label_popup">
+                    <label class= "etiqueta_cajas_popup" for="juguetes_hijo_${i}">Juguetes favoritos</label>
+                    <input class = "caja_escribir" type="text" id="juguetes_hijo_${i}" placeholder="Juguetes favoritos">
+                </div>
+            </div>
+        `;
+        datos_hijos.innerHTML += campo;
+    }
+}
+
 const restricciones_contraseña = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d{2,})(?=.*[!@#$%^&*()_\-+=]).{12,}$/;
 
 function validar_contraseña() {
